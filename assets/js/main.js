f858887e354edd373a9381070cbce5bd7196a824
illy1990 <jascha.hell@outlook.de>
2020-03-17 13:51:58 +0100

    zwischenupdate

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,9 +43,15 @@ console.log(randomNum1_10);
 console.log(randomNum1_100);
 
 // MATH - Ubung 4 - CodeFlow Übung lev1_4: Math.min Math.max
-
-
 console.log(Math.min(393, 205, 479, 47, 376, 267, 385, 2, 190, 383, 286, 462, 115, 138, 331, 409, 427, 245, 224, 276, 483, 55, 147, 177, 208, 347, 135, 282, 33, 270, 475, 426, 476, 453, 474, 351, 219, 358, 354, 410, 240, 139, 371, 356, 277, 202, 264, 204, 447, 488));
 console.log(Math.max(393, 205, 479, 47, 376, 267, 385, 2, 190, 383, 286, 462, 115, 138, 331, 409, 427, 245, 224, 276, 483, 55, 147, 177, 208, 347, 135, 282, 33, 270, 475, 426, 476, 453, 474, 351, 219, 358, 354, 410, 240, 139, 371, 356, 277, 202, 264, 204, 447, 488));
 
+// MATH - Ubung 5 - CodeFlow Übung lev2_1: Math.round()
 
+let RundungsArray = [PI, 9.45, 193.4464, 0.8596433607, 0.65149992, 2.1807301500, - 2.9406290890, - 24.45];
+
+RundungsArray.forEach((ZuRunden) => console.log(Math.ceil(ZuRunden)))
+
+
+
+// MATH - Ubung 6 - CodeFlow Übung lev2_2: Math.random() Math.floor() Game
